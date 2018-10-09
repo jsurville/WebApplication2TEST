b1f607bb9801dc40afc7f9680196f08edd3d5165
jsurville <julien.surville@gmail.com>
2018-10-09 15:13:38 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication2TEST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication2TEST.Startup))]
+namespace WebApplication2TEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
